5b851324dddb38ab361217106a37f9fb15f82b71
Alvin <huerox@msn.com>
2022-03-25 08:33:52 -0700

    added backend url.

--- src/main/java/com/revature/ibrahAzuredemo/AzureDemoApplication.java
+++ src/main/java/com/revature/ibrahAzuredemo/AzureDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IbrahAzureDemoApplication {
+public class AzureDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IbrahAzureDemoApplication.class, args);
+		SpringApplication.run(AzureDemoApplication.class, args);
 	}
 
 }
